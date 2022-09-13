5d2eaba7671954efd065de7d5e4431420b3d6288
Qazi Zubair <qazizubair471@gmail.com>
2022-09-13 15:21:22 +0500

    response message on dm !!have to change ngrok server every time

---
+++ messageReplyer.py
@@ -0,0 +1,30 @@
+from email import message
+from http import client
+import slack
+import os
+from pathlib import Path
+from dotenv import load_dotenv
+from flask import Flask
+from slackeventsapi import SlackEventAdapter
+
+env_path = Path('.')/'.env'
+load_dotenv(dotenv_path=env_path)
+
+
+app = Flask(__name__)
+slack_event_adapter = SlackEventAdapter(os.environ['SIGNING_SECRET'],'/slack/events',app)
+client = slack.WebClient(token=os.environ['Slack_Token'])
+BOT_ID=client.api_call("auth.test")['user_id']
+
+@slack_event_adapter.on('message')
+def message(payload):
+    # print(payload)
+    event = payload.get('event')
+
+    channel_id = event.get('channel')
+    user_id = event.get('user')
+    if BOT_ID!= user_id:
+        client.chat_postMessage(channel =channel_id, text='Hello'  )
+
+if __name__ == '__main__':
+    app.run()
